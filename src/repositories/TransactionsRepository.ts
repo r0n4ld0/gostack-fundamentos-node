e47f39147d781a9c1cc4211082f92b3c3033997c
r0n4ld0 <r0n4ld0@gmail.com>
2020-04-18 16:01:29 -0300

    Desafio: Fundamentos Node.js

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const sumIncomes = this.transactions.reduce((acc, cur) => {
+      return cur.type === 'income' ? acc + cur.value : acc;
+    }, 0);
+
+    const sumOutcomes = this.transactions.reduce((acc, cur) => {
+      return cur.type === 'outcome' ? acc + cur.value : acc;
+    }, 0);
+
+    const balance = {
+      income: sumIncomes,
+      outcome: sumOutcomes,
+      total: sumIncomes - sumOutcomes,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
